3c6a09cf8c28bcc05851642dfae973bc6134de30
Alex Flückiger <alex.flueckiger@gmail.com>
2018-08-27 10:16:52 +0200

    add script to merge all articles of a year into a single txt-file

---
+++ lib/cuttered2single_doc.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Alex Flückiger <alex.flueckiger@uzh.ch>'
+
+# imported modules
+import argparse  # programm argument handling
+import glob
+
+
+def parse_args():
+    """Parses the arguments given with program call"""
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-i', '--input',  required=True, action='store',
+                        dest='input_dir', help='input folder with xml data')
+    parser.add_argument('-o', '--output', required=True,
+                        action='store', dest='out_file', help='output file that collects all sentences of each article')
+
+    return parser.parse_args()
+
+
+def main():
+    """
+    Restructure all cuttered documents of a given input dir and
+    write their sentences into a single txt
+    """
+
+    # parse arguments
+    args = parse_args()
+    input_dir = args.input_dir
+    if input_dir[:-1] != '/':
+        input_dir += '/'
+
+    # create outfile name
+    outfile = args.out_file + '.txt'
+
+    with open(outfile, mode='w', encoding='utf-8') as f_out:
+        # iterate over relevant files in the given input folder
+        for infile in glob.iglob(input_dir+'/**/*cuttered.sent.txt', recursive=True):
+
+            # write file or dir id
+            f_out.write('{}\n'.format(input_dir))
+
+            with open(infile, mode='r', encoding='utf-8') as f_in:
+                # write article id
+                f_out.write(infile + '\n')
+                for line in f_in:
+                    f_out.write(line.strip() + '\n')
+
+                f_out.write('.EOA\n')
+            f_out.write('.EOB\n')
+
+
+if __name__ == '__main__':
+
+    main()
