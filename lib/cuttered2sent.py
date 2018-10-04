20fe2d80484525fb70877048fc2708092ede1730
Alex Flückiger <alex.flueckiger@gmail.com>
2018-10-04 18:03:03 +0200

    correct wrong progress indication

--- lib/cuttered2sent.py
+++ lib/cuttered2sent.py
@@ -30,17 +30,20 @@ def process(a):
     sentfinal = set(OPTIONS['sentfinal'])
     with open(a, encoding='utf-8') as f:
         sent = []
-        for i, l in enumerate(f):
+        i = 0
+        for l in f:
             l = l.rstrip()
             if l != '':
                 sent.append(l)
+                i += 1
             if l in sentfinal or i > 250:
-                # limit maximal length of sentence to avoid problems in machine translation
+                # limit maximal length of sentence to 250 tokens
+                # to avoid problems in machine translation
                 print(' '.join(sent))
                 sent = []
+                i = 0
 
         # print the remainings of a sentence at the end of a document
-        # this is is important for the disclaimer of confidential articles
         if len(sent) > 0:
             print(' '.join(sent))
 
