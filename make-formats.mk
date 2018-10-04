775ada78885473856e43ce2e4f1eca56aacc2987
Alex Flückiger <alex.flueckiger@gmail.com>
2018-10-04 17:55:24 +0200

    writing all articles into single file

--- make-formats.mk
+++ make-formats.mk
@@ -8,14 +8,9 @@ tettext-$(FILE_LANG)-by-year-files:=$(wildcard $(DATA_DIR)/$(FILE_LANG)/$(YEAR)/
 cuttered-$(FILE_LANG)-text-files:=$(tettext-$(FILE_LANG)-by-year-files:.text=.cuttered.txt)
 sent-$(FILE_LANG)-text-files:=$(tettext-$(FILE_LANG)-by-year-files:.text=.cuttered.sent.txt)
 
-cuttered-$(FILE_LANG)-text-target: $(cuttered-$(FILE_LANG)-text-files) $(sent-$(FILE_LANG)-text-files)
+#cuttered-$(FILE_LANG)-text-target: $(cuttered-$(FILE_LANG)-text-files) $(sent-$(FILE_LANG)-text-files)
 
-#$(FILE_OUT)-target:	$(FILE_OUT)
-
-
-$(info $$var is $(FILE_OUT))
-#$(info $$var is $(tettext-$(FILE_LANG)-by-year-files))
-#$(info $$var is $(cuttered-$(FILE_LANG)-text-files))
+# $(info $$var is $(FILE_OUT))
 
 # The output of the cutter doesn't isolate all abbreviations properly.
 # Hence, we apply post-processing to merge short tokens (up to 4-gram) and
@@ -30,7 +25,7 @@ $(info $$var is $(FILE_OUT))
 	python3 lib/cuttered2sent.py $< > $@
 
 #write all documents into a single textfile using separator tags
-$(FILE_OUT): %.cuttered.sent.txt
+$(FILE_OUT): $(cuttered-$(FILE_LANG)-text-files) $(sent-$(FILE_LANG)-text-files)
 	python3 lib/cuttered2single_doc.py -i $(DATA_DIR)/$(FILE_LANG)/$(YEAR) -o $@
 
 
