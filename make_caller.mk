cddd372d7333d7ea23290609e278b4bdac78b972
Alex Flückiger <alex.flueckiger@gmail.com>
2018-10-24 09:01:31 +0200

    correct bug with doubled file extension

--- make_caller.mk
+++ make_caller.mk
@@ -15,12 +15,14 @@ de-translated-doc-targets: $(de-translated-doc-files) $(de-single-doc-files)
 
 # DE: process extracted text data anually and language-wise
 de_%_all.txt:
-	$(MAKE) -f make-formats.mk YEAR=$* FILE_LANG=de DIR_IN=$(DIR_IN)
+	$(MAKE) -f make-formats.mk YEAR=$* FILE_LANG=de DIR_IN=$(DIR_IN) -j 20
 
+# Translate all german docs to French with Moses.
+# Remove square brackets as Moses cannot process them
 de_fr_%_all.txt: de_%_all.txt
 	cat $(DIR_IN)/$< | \
 	/mnt/storage/clfiles/resources/applications/mt/moses/vGitHub/scripts/tokenizer/lowercase.perl | \
-	sed -r "s/\[//" | sed -r "s/\]//" | \ # remove square brackets as Moses cannot process them
-	moses -f mt_moses/train_de_fr/binarised_model/moses.ini -threads 2 | \
+	sed -r "s/\[//" | sed -r "s/\]//" | \
+	moses -f mt_moses/train_de_fr/binarised_model/moses.ini -threads 10 | \
 	sed -r "s/^\.eoa/.EOA/" \
-	> $(DIR_IN)/$@
+	> $(DIR_OUT)/$@
