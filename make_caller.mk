aff6deb8ccce9ff81b5aebebee6c594e56f1f236
Alex Flückiger <alex.flueckiger@gmail.com>
2019-04-09 10:45:30 +0200

    clean up and comments

--- make_caller.mk
+++ make_caller.mk
@@ -14,25 +14,27 @@ de-fr-alignments-doc-files:=$(patsubst %, $(DIR_OUT)/de_fr_%_alignments.xml, $(Y
 de-fr-aligned-doc-files:=$(patsubst %, $(DIR_OUT)/de_fr_%_aligned_docs.tsv, $(YEARS))
 de-fr-parallel-corpus-files:=$(patsubst %, $(DIR_OUT)/de_fr_%_parallel_corpus.txt, $(YEARS))
 
-de-fr-alignments-doc-targets: $(de-single-doc-files) $(fr-single-doc-files) $(de-translated-doc-files) $(de-fr-alignments-doc-files) overview_stats_alignment.csv
+de-fr-translated-dummy:=$(patsubst %, $(DIR_OUT)/de_fr_%_trans_dummy.txt, $(YEARS))
 
-de-fr-alignments-sent-targets: $(de-fr-parallel-corpus-files) de-fr-alignments-doc-targets
+de-fr-alignments-targets: $(de-single-doc-files) $(fr-single-doc-files) $(de-translated-doc-files) $(de-fr-alignments-doc-files) overview_stats_alignment.csv $(de-fr-parallel-corpus-files)
+
 
 #### TOKENIZING ###
+# segment and tokenize extracted text data anually and language-wise
 
+# DE
 de_%_all.txt:
-	# DE: process extracted text data anually and language-wise
 	$(MAKE) -f make-formats.mk YEAR=$(subst $(DIR_OUT)/,,$*)  FILE_LANG=de DIR_IN=$(DIR_IN) DIR_OUT=$(DIR_OUT)
 
+# FR
 fr_%_all.txt:
-	# FR: process extracted text data anually and language-wise
 	$(MAKE) -f make-formats.mk YEAR=$(subst $(DIR_OUT)/,,$*) FILE_LANG=fr DIR_IN=$(DIR_IN) DIR_OUT=$(DIR_OUT)
 
 #### DOCUMENT ALIGMNENT ###
 
+# Translate all German docs to French with Moses
+# Remove square brackets as Moses cannot process them
 de_fr_%_all.txt: de_%_all.txt
-	# Translate all German docs to French with Moses.
-	# Remove square brackets as Moses cannot process them
 	cat $< | \
 	/mnt/storage/clfiles/resources/applications/mt/moses/vGitHub/scripts/tokenizer/lowercase.perl | \
 	sed -r "s/\[//g" | sed -r "s/\]//g" | \
@@ -40,14 +42,13 @@ de_fr_%_all.txt: de_%_all.txt
 	sed -r "s/^\.eoa/.EOA/" \
 	> $@
 
+# Compute BLEU-alignments for German and French documents
 de_fr_%_alignments.xml: de_%_all.txt fr_%_all.txt de_fr_%_all.txt
-	# Compute BLEU-alignments for German and French documents
 	python3 lib/bleualign_articles.py -src $(word 1, $^) -trg $(word 2, $^) \
 	-t $(word 3, $^) -o $@ -c
 
-
+# collect all alignment stats and merge them into single csv
 overview_stats_alignment.csv: de_fr_%_alignments.xml
-	# collect all alignment stats and merge them into single csv
 	head -n 1 *.csv |  sed '2q;d' > csv_header.txt
 	awk 'FNR > 1' *.csv > total_stats_alignment.csv
 	cat csv_header.txt total_stats_alignment.csv > overview_stats_alignment.csv
@@ -56,20 +57,18 @@ overview_stats_alignment.csv: de_fr_%_alignments.xml
 
 #### SENTENCE ALIGMNENT ###
 
-.translation2docs: de_fr_%_all.txt
-	# Create temporary folder in RAM
-	PATH="$(DIR_TRANS)/$(subst $(DIR_OUT)/,,$*)"
-	mkdir -p $(PATH)
-	# Firstly, remove first and last line from the translation file.
-	# Secondly, split this file into its articles at .EOA delimiter and write into RAM.
-    sed '1d; $d' $< | csplit -z --digits=4  --quiet --suppress-matched --prefix=$(PATH)/trans_art /dev/stdin "/.EOA/"  "{*}"
-    # Rename all article according to its first line
-    for i in $(PATH)/trans_art* ; do mv -n "$i" "$(basename $(cat "$i"|head -n1))"; done
+# Firstly, remove first and last line from the translation file.
+# Secondly, split this file into its articles at .EOA delimiter and write into RAM.
+# Thirdly, rename all article according to its first line
+de_fr_%_trans_dummy.txt: de_fr_%_all.txt
+	mkdir -p $(PATH)/$(subst $(DIR_OUT)/,,$*) && \
+    sed '1d; $d' $< | csplit -z --digits=4  --quiet --suppress-matched --prefix=$(PATH)/$(subst $(DIR_OUT)/,,$*)/trans_art /dev/stdin "/.EOA/"  "{*}" && \
+    for i in $(PATH)/$(subst $(DIR_OUT)/,,$*)/trans_art*; do mv -n "$i" "$(basename $(cat "$i"|head -n1))"; done
 
+# Create a tsv-file with all aligned articles (src, trg, translation)
 de_fr_%_aligned_docs.tsv: de_fr_%_alignments.xml .translation2docs
-	# Create with aligned articles (src, trg, translation)
 	python lib/aligned2tsv.py -i $< -o $(word 1, $^) -t $(DIR_TRANS)
 
-de_fr_%_parallel_corpus.txt: de_fr_%_aligned_docs.txt
-	# Align sentences with bleu-champ
+# Align sentences with bleu-champ
+de_fr_%_parallel_corpus.txt: de_fr_%_aligned_docs.tsv
 	while IFS=$'\t' read -r -a article;	do bleu-champ -q -s ${article[2]} -t ${article[1]} -S ${article[0]} >> $@; done < $<
