de679979626f7a0c09b016ad2e281cc4a16528c0
Alex Flückiger <alex.flueckiger@gmail.com>
2019-08-26 18:01:37 +0200

    add spaCy processing (alternative to cutter)

--- make-formats.mk
+++ make-formats.mk
@@ -8,18 +8,23 @@ DIR_OUT?= data_alignment
 FILE_OUT?= $(DIR_OUT)/$(FILE_LANG)_$(YEAR)_all.txt
 
 
+
+### SEGMENTATION AND TOKENIZATION WITH cutter
 # Make targets for formats derived from the text output
 tettext-$(FILE_LANG)-by-year-files:=$(wildcard $(DIR_IN)/$(FILE_LANG)/$(YEAR)/*/*.text)
 
 cuttered-$(FILE_LANG)-text-files:=$(tettext-$(FILE_LANG)-by-year-files:.text=.cuttered.txt)
+cuttered-$(FILE_LANG)-text-target: $(cuttered-$(FILE_LANG)-text-files)
+
 sent-$(FILE_LANG)-text-files:=$(tettext-$(FILE_LANG)-by-year-files:.text=.cuttered.sent.txt)
+sent-$(FILE_LANG)-text-target: $(sent-$(FILE_LANG)-text-files)
 
-trans-$(FILE_LANG)-text-files:=$(tettext-$(FILE_LANG)-by-year-files:.text=.cuttered.sent.trans.txt)
+single-doc-year-file= $(DIR_OUT)/$(FILE_LANG)_$(YEAR)_all.txt
+single-doc-year-target: $(single-doc-year-file)
 
 
-#$(FILE_OUT): $(cuttered-$(FILE_LANG)-text-files) $(sent-$(FILE_LANG)-text-files)
+print-%: ; @echo $* is $($*)
 
-# $(info $$var is $(FILE_OUT))
 
 # TODO: The output of the cutter doesn't isolate all abbreviations properly.
 # Hence, we apply post-processing to merge short tokens (up to 4-gram) and
@@ -33,11 +38,23 @@ trans-$(FILE_LANG)-text-files:=$(tettext-$(FILE_LANG)-by-year-files:.text=.cutte
 
 #write all documents into a single textfile using separator tags
 # TODO: Recheck statement as it is probably incorrect (always executed)
-$(FILE_OUT): $(cuttered-$(FILE_LANG)-text-files) $(sent-$(FILE_LANG)-text-files)
+#$(FILE_OUT): $(cuttered-$(FILE_LANG)-text-files) $(sent-$(FILE_LANG)-text-files)
+%_all.txt: $(sent-$(FILE_LANG)-text-files)
 	mkdir -p $(DIR_OUT) && \
 	python3 lib/cuttered2single_doc.py -i $(DIR_IN)/$(FILE_LANG)/$(YEAR) -w /**/*cuttered.sent.txt -o $@
 
+### SEGMENTATION AND TOKENIZATION WITH spaCy
+# TODO: year-wise processing in order to parallelize. Since all entities
 
+tettext-$(FILE_LANG)-all-files:=$(wildcard $(DIR_IN)/$(FILE_LANG)/*/*/*.text)
+sent-$(FILE_LANG)-spacy-text-files:=$(tettext-$(FILE_LANG)-all-files:.text=.spacy.sent.txt)
 
+$(sent-$(FILE_LANG)-spacy-text-files): sent-$(FILE_LANG)-spacy-text-files-batch-process
+sent-$(FILE_LANG)-spacy-text-target: $(sent-$(FILE_LANG)-spacy-text-files)
+
+# flag -u to avoid output buffering
+# additionally, extract all name entities and count their occurrence
+sent-$(FILE_LANG)-spacy-text-files-batch-process: lib/processing_spacy.py
+	python3 -u lib/processing_spacy.py -dir $(DIR_IN)/$(FILE_LANG) -l $(FILE_LANG) -f_ne named_entities_$(FILE_LANG).tsv
 
 SHELL:=/bin/bash
