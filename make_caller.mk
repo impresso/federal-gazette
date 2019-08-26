7852ed78bfe6278aace57069afa0304f4a4b10b8
Alex Flückiger <alex.flueckiger@gmail.com>
2019-08-26 18:08:07 +0200

    use EuroParl corpus for multilingual embeddings and change hyperparameters

--- make_caller.mk
+++ make_caller.mk
@@ -32,13 +32,13 @@ it-single-doc-target: $(it-single-doc-files)
 all-single-doc-target: de-single-doc-target fr-single-doc-target it-single-doc-target
 
 de_%_all.txt:
-	$(MAKE) -f make-formats.mk YEAR=$(subst $(DIR_ALIGN)/,,$*)  FILE_LANG=de DIR_IN=$(DIR_IN) DIR_OUT=$(DIR_ALIGN)
+	$(MAKE) -f make-formats.mk YEAR=$(subst $(DIR_ALIGN)/,,$*) FILE_LANG=de DIR_IN=$(DIR_IN) DIR_OUT=$(DIR_ALIGN) single-doc-year-target
 
 fr_%_all.txt:
-	$(MAKE) -f make-formats.mk YEAR=$(subst $(DIR_ALIGN)/,,$*) FILE_LANG=fr DIR_IN=$(DIR_IN) DIR_OUT=$(DIR_ALIGN)
+	$(MAKE) -f make-formats.mk YEAR=$(subst $(DIR_ALIGN)/,,$*) FILE_LANG=fr DIR_IN=$(DIR_IN) DIR_OUT=$(DIR_ALIGN) single-doc-year-target
 
 it_%_all.txt:
-	$(MAKE) -f make-formats.mk YEAR=$(subst $(DIR_ALIGN)/,,$*) FILE_LANG=it DIR_IN=$(DIR_IN) DIR_OUT=$(DIR_ALIGN)
+	$(MAKE) -f make-formats.mk YEAR=$(subst $(DIR_ALIGN)/,,$*) FILE_LANG=it DIR_IN=$(DIR_IN) DIR_OUT=$(DIR_ALIGN) single-doc-year-target
 
 
 
@@ -60,7 +60,8 @@ de_fr_%_all.txt: de_%_all.txt
 	moses -f mt_moses/train_de_fr/binarised_model/moses.ini -v 0 -threads 4 | \
 	perl /mnt/storage/clfiles/resources/applications/mt/moses/vGitHub/scripts/tokenizer/deescape-special-chars.perl | \
 	sed -r "s/^\.eoa/.EOA/" \
-	> $@  \
+	sed -r "s/^\.eob/.EOB/" \
+	> $@ \
 	|| echo 'Error during Moses translation process for the following year: ' $@ >> log_moses.txt
 
 # TODO: translate documents from German to Italian
@@ -79,7 +80,7 @@ de_fr_%_alignments.xml: de_%_all.txt fr_%_all.txt de_fr_%_all.txt
 
 de-fr-alignments-stats-files:=$(de-fr-alignments-doc-files:.xml=_stats.csv)
 
-de-fr-alignments-stats-target: $(de-fr-alignments-stats-files) de_fr_overview_stats_alignment.csv
+de-fr-alignments-stats-target: $(de-fr-alignments-stats-files) $(DIR_ALIGN)/de_fr_overview_stats_alignment.csv
 
 $(DIR_ALIGN)/de_fr_overview_stats_alignment.csv: $(de-fr-alignments-stats-files)
 	head -1 $< > $<.header.temp
@@ -115,22 +116,19 @@ de_fr_%_aligned_docs.tsv: de_fr_%_alignments.xml de_fr_%_trans_dummy.txt
 # Align sentences with bleu-champ and set EOA-marker to indicate the article boundaries
 # After the alignment, clean up directory with translated files
 de-fr-parallel-corpus-files:=$(patsubst %, $(DIR_ALIGN)/de_fr_%_parallel_corpus.tsv, $(YEARS))
-
 de-fr-parallel-corpus-target: $(de-fr-parallel-corpus-files)
 
 de_fr_%_parallel_corpus.tsv: de_fr_%_aligned_docs.tsv
-	while IFS=$$'\t' read -r col_src col_trg col_trans ; do  bleu-champ -q -s $${col_trans} -t $${col_trg} -S $${col_src} >> $@ ; echo '.EOA\t.EOA' >> $@; done < $<
+	while IFS=$$'\t' read -r col_src col_trg col_trans ; do bleu-champ -q -s $${col_trans} -t $${col_trg} -S $${col_src} >> $@ ; echo '.EOA\t.EOA' >> $@; done < $<
 #	rm -r "$(DIR_TRANS)/$*"
 
 
 # Create a parallel corpus across years as tsv file
 $(DIR_ALIGN)/de_fr_all_parallel_corpus.tsv: $(de-fr-parallel-corpus-files)
-	cat  $^ > $@
+	cat $^ > $@
 
 
 # Split the parallel corpus into separate files per language
-all_sent_parallel-target: $(DIR_ALIGN)/de_fr_sent_parallel_de.txt $(DIR_ALIGN)/de_fr_sent_parallel_fr.txt
-
 $(DIR_ALIGN)/de_fr_sent_parallel.de: $(DIR_ALIGN)/de_fr_all_parallel_corpus.tsv
 	cut -f1 $< > $@
 
@@ -144,22 +142,52 @@ $(DIR_ALIGN)/de_fr_sent_parallel_filtered.de: $(DIR_ALIGN)/de_fr_sent_parallel.d
 $(DIR_ALIGN)/de_fr_sent_parallel_filtered.fr: $(DIR_ALIGN)/de_fr_sent_parallel_filtered.de
 
 
+
 #### MULTILINGUAL EMBEDDINGS ###
 DIR_EMBED?= embedding
 DIR_EMBED_DATA?= embedding/data
+DIR_EUROPARL?= europarl_data
 
-# Prepare data (French is cleaned implicit)
-$(DIR_EMBED_DATA)/de_fr_sent_parallel_clean.de: $(DIR_ALIGN)/de_fr_sent_parallel_filtered.de $(DIR_ALIGN)/de_fr_sent_parallel_filtered.fr
+# Tokenize Europarl corpus (perl command to retrieve line-based structure)
+europarl-de-fr-files: $(DIR_EUROPARL)/Europarl.de-fr.de $(DIR_EUROPARL)/Europarl.de-fr.fr
+europarl-de-fr-cuttered-files:= $(DIR_EUROPARL)/Europarl.de-fr.cuttered.de $(DIR_EUROPARL)/Europarl.de-fr.cuttered.fr
+europarl-de-fr-cuttered-target: $(europarl-de-fr-cuttered-files)
+
+Europarl.de-fr.cuttered.%: Europarl.de-fr.%
+	cat $< | cutter $(*F) -T | perl -p -e "s/(?<=.)\n/ /g" | sed "/^$$/d" > $@
+
+# Merge with EuroParl corpus
+de_fr_parallel_fedgaz_europarl-files:= $(DIR_EMBED_DATA)/de_fr_parallel_fedgaz_europarl.de $(DIR_EMBED_DATA)/de_fr_parallel_fedgaz_europarl.fr
+# TODO: weirdly, here the dir is needed, otherwise the target rule is not found
+#de_fr_parallel_fedgaz_europarl.%: de_fr_sent_parallel_filtered.% Europarl.de-fr.cuttered.%
+$(DIR_EMBED_DATA)/de_fr_parallel_fedgaz_europarl.%: $(DIR_ALIGN)/de_fr_sent_parallel_filtered.% $(DIR_EUROPARL)/Europarl.de-fr.cuttered.%
+	cat $^ > $@
+
+# Prepare data (French is cleaned implicitly)
+de-fr-sent-parallel-clean-de-files:=$(DIR_EMBED_DATA)/de_fr_parallel_clean.de
+de-fr-sent-parallel-clean-fr-files:=$(DIR_EMBED_DATA)/de_fr_parallel_clean.fr
+de-fr-sent-parallel-clean-files:= $(de-fr-sent-parallel-clean-de-files) $(de-fr-sent-parallel-clean-fr-files)
+
+$(de-fr-sent-parallel-clean-de-files): $(de_fr_parallel_fedgaz_europarl-files)
 	python2 lib/multivec_scripts/prepare-data.py $(<:.de=) $(@:.de=) de fr \
-	--lowercase --normalize-digits --normalize-punk --min-count 10 --shuffle --script lib/multivec_scripts --threads 4 --verbose
-$(DIR_EMBED_DATA)/de_fr_sent_parallel_clean.fr: $(DIR_EMBED_DATA)/de_fr_sent_parallel_clean.de
+	--lowercase --normalize-digits --min-count 5 --shuffle --script lib/multivec_scripts --threads 10 --verbose
+$(de-fr-sent-parallel-clean-fr-files): $(de_fr_parallel_fedgaz_europarl-files)
+
+# keep only alpha-numeric character, underscore, and hyphen
+de-fr-sent-parallel-clean-nopunc-files:= $(DIR_EMBED_DATA)/de_fr_parallel_clean_nopunc.de $(DIR_EMBED_DATA)/de_fr_parallel_clean_nopunc.fr
+
+# remove all special characters if preceded by a initial space (i.e. if they are a token)
+de_fr_parallel_clean_nopunc.%: de_fr_parallel_clean.%
+	cat $< | sed "s/ [^[:alnum:] \_-]\+/ /g" | sed "s/ \+/ /g" > $@
+
+de-fr-prepare-embedding-data-target: $(de-fr-sent-parallel-clean-nopunc-files) $(de_fr_parallel_fedgaz_europarl-files)
 
 # Train multivec models with dimension 100
 vectors-100-de-fr-target: $(DIR_EMBED)/vectors.100.de-fr.de.vec $(DIR_EMBED)/vectors.100.de-fr.fr.vec $(DIR_EMBED)/vectors.100.de-fr.de.eval.txt
 
-$(DIR_EMBED)/biskip.100.de-fr.bin: $(DIR_EMBED_DATA)/de_fr_sent_parallel_clean.de $(DIR_EMBED_DATA)/de_fr_sent_parallel_clean.fr
+$(DIR_EMBED)/biskip.100.de-fr.bin: $(de-fr-sent-parallel-clean-nopunc-files)
 	mkdir -p $(@D) && \
-	multivec-bi --train-src $(word 1, $^) --train-trg $(word 2, $^) --dimension 100 --min-count 10 --window-size 5 --threads 10 --iter 10 --sg --save $@
+	multivec-bi --train-src $(word 1, $^) --train-trg $(word 2, $^) --dimension 100 --min-count 10 --window-size 10 --threads 10 --iter 20 --sg --save $@
 
 $(DIR_EMBED)/biskip.100.de-fr.de.bin: $(DIR_EMBED)/biskip.100.de-fr.bin
 	multivec-bi --load $< --save-src $@
@@ -179,9 +207,9 @@ $(DIR_EMBED)/vectors.100.de-fr.de.eval.txt: $(DIR_EMBED)/vectors.100.de-fr.de.ve
 # Train and evaluate multivec models with dimension 300
 vectors-300-de-fr-target: $(DIR_EMBED)/vectors.300.de-fr.de.vec $(DIR_EMBED)/vectors.300.de-fr.fr.vec $(DIR_EMBED)/vectors.300.de-fr.de.eval.txt
 
-$(DIR_EMBED)/biskip.300.de-fr.bin: $(DIR_EMBED_DATA)/de_fr_sent_parallel_clean.de $(DIR_EMBED_DATA)/de_fr_sent_parallel_clean.fr
+$(DIR_EMBED)/biskip.300.de-fr.bin: $(de-fr-sent-parallel-clean-nopunc-files)
 	mkdir -p $(@D) && \
-	multivec-bi --train-src $(word 1, $^) --train-trg $(word 2, $^) --dimension 300 --min-count 10 --window-size 5 --threads 10 --iter 10 --sg --save $@
+	multivec-bi --train-src $(word 1, $^) --train-trg $(word 2, $^) --dimension 300 --min-count 10 --window-size 10 --threads 10 --iter 20 --sg --save $@
 
 $(DIR_EMBED)/biskip.300.de-fr.de.bin: $(DIR_EMBED)/biskip.300.de-fr.bin
 	multivec-bi --load $< --save-src $@
