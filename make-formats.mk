242060bb249a6e965fef7be0faf079b44b6da8c6
Alex Flückiger <alex.flueckiger@gmail.com>
2020-03-10 12:01:37 +0100

    clean up recipes

--- make-formats.mk
+++ make-formats.mk
@@ -3,34 +3,51 @@ SHELL:=/bin/bash
 # ensure that entire pipe command fails if a part fails
 export SHELLOPTS:=errexit:pipefail
 
-DIR_IN?= data_text
-DIR_OUT?= data_alignment
-FILE_OUT?= $(DIR_OUT)/$(FILE_LANG)_$(YEAR)_all.txt
+ALIGN_DIR?= data_alignment
+PDF_DIR?= data_pdf
+TEXT_DIR?= data_text
+
 
 # define source name with an capitalized suffix of the language
 SRC_NAME?= FedGaz$(shell FILE_LANG=$(FILE_LANG); echo $${FILE_LANG^})
-
-### SEGMENTATION AND TOKENIZATION WITH cutter
-# Make targets for formats derived from the text output
-tettext-$(FILE_LANG)-by-year-files:=$(wildcard $(DIR_IN)/$(SRC_NAME)/$(YEAR)/*/*/*.text)
-
-cuttered-$(FILE_LANG)-text-files:=$(tettext-$(FILE_LANG)-by-year-files:.text=.cuttered.txt)
-cuttered-$(FILE_LANG)-text-target: $(cuttered-$(FILE_LANG)-text-files)
-
-sent-$(FILE_LANG)-text-files:=$(tettext-$(FILE_LANG)-by-year-files:.text=.cuttered.sent.txt)
-sent-$(FILE_LANG)-text-target: $(sent-$(FILE_LANG)-text-files)
-
-single-doc-year-file= $(DIR_OUT)/$(FILE_LANG)_$(YEAR)_all.txt
-single-doc-year-target: $(single-doc-year-file)
 
 
 print-%: ; @echo $* is $($*)
 
 # self-documenting makefile
-help : make-formats.mk
+help: make-formats.mk
 	@sed -n 's/^##//p' $<
 
+### EXTRACTING TEXT FROM PDFs WITH tet
+pdf-files:=$(wildcard $(PDF_DIR)/$(SRC_NAME)/$(YEAR)/*/*/*.pdf)
+text-files:=$(subst $(PDF_DIR),$(TEXT_DIR),$(pdf-files:.pdf=.text))
+text-target: $(text-files)
+
+
+$(TEXT_DIR)/%.text:$(PDF_DIR)/%.pdf
+	mkdir -p $(@D) && \
+	if [[ $$(basename $(<D) ) < "1999-06-22" ]] ;\
+	then\
+		tet --text --lastpage last-1 --outfile $@ -v 3 $< > $@.log ; \
+	else \
+		tet --text --outfile $@ -v 3 $< > $@.log ; \
+	fi
+
+
+
+### SEGMENTATION AND TOKENIZATION WITH cutter
+# Make targets for formats derived from the text output
+cuttered-text-files:=$(text-files:.text=.cuttered.txt)
+cuttered-text-target: $(cuttered-text-files)
+
+sent-text-files:=$(text-files:.text=.cuttered.sent.txt)
+sent-text-target: $(sent-text-files)
+
+
+
+### TOKENIZATION and SENTENCE SEGMENTATION WITH cutter
 ## .cuttered.txt	: Tokenized text with one token per line
+# filtering printed page numbers with perl
 %.cuttered.txt: %.text
 	perl -lne 's/\d*\s*\f\s*\d*//;print' < $< | cutter $(FILE_LANG) -T  > $@
 
@@ -40,26 +57,31 @@ help : make-formats.mk
 
 ## _all.txt	: Concat all documents per year using separator tags
 # TODO: Recheck statement as it is probably incorrect (always executed)
-#$(FILE_OUT): $(cuttered-$(FILE_LANG)-text-files) $(sent-$(FILE_LANG)-text-files)
-%_all.txt: $(sent-$(FILE_LANG)-text-files)
+single-doc-year-file= $(ALIGN_DIR)/$(FILE_LANG)_$(YEAR)_all.txt
+single-doc-year-target: $(single-doc-year-file)
+
+%_all.txt: $(sent-text-files)
 	mkdir -p $(@D) && \
-	python3 lib/cuttered2single_doc.py -i $(DIR_IN)/$(SRC_NAME)/$(YEAR) -w /**/*cuttered.sent.txt -o $@
+	python3 lib/cuttered2single_doc.py -i $(TEXT_DIR)/$(SRC_NAME)/$(YEAR) -w /**/*cuttered.sent.txt -o $@
+
 
 ### SEGMENTATION AND TOKENIZATION WITH spaCy
 # TODO: year-wise processing in order to parallelize.
 
-tettext-$(FILE_LANG)-all-files:=$(wildcard $(DIR_IN)/$(FILE_LANG)/*/*/*.text)
-sent-$(FILE_LANG)-spacy-text-files:=$(tettext-$(FILE_LANG)-all-files:.text=.spacy.sent.txt)
+pdf-all-year-files:=$(wildcard $(PDF_DIR)/$(SRC_NAME)/$(YEAR)/*/*/*.pdf)
+text-all-year-files:=$(subst $(PDF_DIR),$(TEXT_DIR),$(pdf-all-year-files:.pdf=.text))
 
-$(sent-$(FILE_LANG)-spacy-text-files): sent-$(FILE_LANG)-spacy-text-files-batch-process
-sent-$(FILE_LANG)-spacy-text-target: $(sent-$(FILE_LANG)-spacy-text-files)
+sent-spacy-text-files:=$(text-all-year-files:.text=.spacy.sent.txt)
+
+$(sent-spacy-text-files): sent-spacy-$(SRC_NAME)-text-files-batch-process
+sent-%-spacy-text-target: $(sent-spacy-text-files)
 
 # flag -u to avoid output buffering
 # additionally, extract all name entities and count their occurrence
 
 ## .spacy.sent.txt	: Tokenized text with one sentence per line and tokens seperated by space
-sent-$(FILE_LANG)-spacy-text-files-batch-process: lib/processing_spacy.py
-	python3 -m spacy download $(FILE_LANG)_core_news_md
-	python3 -u lib/processing_spacy.py -dir $(DIR_IN)/$(FILE_LANG) -l $(FILE_LANG) -f_ne named_entities_$(FILE_LANG).tsv
+sent-%-spacy-text-files-batch-process: lib/processing_spacy.py
+	python3 -m spacy download $*_core_news_md
+	python3 -u lib/processing_spacy.py -dir $(TEXT_DIR)/$* -l $* -f_ne named_entities_$*.tsv
 
 SHELL:=/bin/bash
