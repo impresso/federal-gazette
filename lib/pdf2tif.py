9fa78bfbd8c237819938bc51e574efade95ea89f
Alex Flückiger <alex.flueckiger@gmail.com>
2020-09-21 14:54:34 +0200

    assemble a script instead of executing directly

--- lib/pdf2tif.py
+++ lib/pdf2tif.py
@@ -3,7 +3,7 @@
 
 
 """
-
+Create Bash scripts to extract and rename TIF images from scanned documents
 """
 
 __author__ = "Alex Flückiger"
@@ -15,11 +15,19 @@ __status__ = "development"
 
 import argparse
 import pandas as pd
-import os
+
+RENAME = """
+a=1
+for i in `ls -v {0}/*.tif`; do
+  new=$(printf "{1}-p%04d.tif" "$a")
+  mv "$i" "{0}/$new"
+  a=$((a+1))
+done
+"""
 
 
 def parse_args():
-    """parses the arguments given with program call"""
+    """Parse the arguments given with program call"""
 
     parser = argparse.ArgumentParser()
 
@@ -32,58 +40,76 @@ def parse_args():
         help="input file with tab-separated metadata",
     )
 
+    parser.add_argument(
+        "-t",
+        "--tet",
+        required=True,
+        action="store",
+        dest="f_tet",
+        help="output file into which tet commands are written",
+    )
+
+    parser.add_argument(
+        "-r",
+        "--rename",
+        required=True,
+        action="store",
+        dest="f_rename",
+        help="output file into which renaming commands are written",
+    )
+
     return parser.parse_args()
 
 
-def extract_tif_via_bash(f_in):
+def create_bash_scripts(fname_in, fname_tet, fname_rename):
     """
-    Extract tif images from all the pdfs and rename them according to the canonical naming scheme.
-    The Bash commands are assembled in Python and written to stdout.
-    """
-
-    rename = """
-    a=1
-    for i in `ls -v {0}/*`; do
-      new=$(printf "{1}-p%04d.tif" "$a")
-      mv "$i" "{0}/$new"
-      a=$((a+1))
-    done
+    Write two Bash scripts to extract and rename the tif images respectively.
+    The script uses the canonical schema, used in the Impresso project.
     """
 
-    df = pd.read_csv(f_in, sep="\t", parse_dates=["issue_date"])
+    df = pd.read_csv(fname_in, sep="\t", parse_dates=["issue_date"])
 
-    # remove potential existing data of this newspaper
-    trg_dir = "/".join(df.loc[0, "canonical_dir_tif"].split("/")[:2])
-    os.system("rm -rf " + trg_dir)
+    with open(fname_tet, mode="w") as f_tet, open(fname_rename, mode="w") as f_rename:
+        # remove potential existing data of this newspaper
+        trg_dir = "/".join(df.loc[0, "canonical_dir_tif"].split("/")[:2])
+        # os.system("rm -rf " + trg_dir)
+        f_tet.write("rm -rf " + trg_dir + "\n")
 
-    for i, row in df.iterrows():
+        # Restrict to period for which scanned documents with OCR exists.
+        # Starting from 22 June 1999, documents are fully digital
+        for i, row in df[df.issue_date < "1999-06-22"].iterrows():
 
-        mkdir = "mkdir -p " + row["canonical_dir_tif"]
-        os.system(mkdir + "\n")
+            mkdir = "mkdir -p " + row["canonical_dir_tif"]
+            # os.system(mkdir + "\n")
+            f_tet.write(mkdir + "\n")
 
-        # create tif files from pdf if document has a length of 1 at minimum
-        if row["page_count_full"] > 0:
-            tet = "tet --targetdir {} --image --lastpage {} {}".format(
-                row["canonical_dir_tif"], row["page_count_full"], row["pdf_path"]
-            )
-            os.system(tet + "\n")
+            # create tif files from pdf if document has a length of 1 at minimum
+            if row["page_count_full"] > 0:
+                tet = "tet --targetdir {} --image --lastpage {} {}".format(
+                    row["canonical_dir_tif"], row["page_count_full"], row["pdf_path"]
+                )
+                # os.system(tet + "\n")
+                f_tet.write(tet + "\n")
 
-        # rename the created files according to the canonical scheme after each day (change of directory)
-        stem = "-".join(row["canonical_dir_tif"].split("/")[1:])
-        try:
-            if row["canonical_dir_tif"] != df.loc[i + 1, "canonical_dir_tif"]:
-                os.system(rename.format(row["canonical_dir_tif"], stem) + "\n")
-        except KeyError:
-            # do also the renaming after the last article
-            os.system(rename.format(row["canonical_dir_tif"], stem) + "\n")
+            # rename the created files according to the canonical scheme after each day (change of directory)
+            stem = "-".join(row["canonical_dir_tif"].split("/")[1:])
+            try:
+                if row["canonical_dir_tif"] != df.loc[i + 1, "canonical_dir_tif"]:
+                    # os.system(rename.format(row["canonical_dir_tif"], stem) + "\n")
+                    rename_issue = RENAME.format(row["canonical_dir_tif"], stem)
+                    f_rename.write(rename_issue + "\n")
+
+            except KeyError:
+                # do also the renaming after the last article
+                # os.system(rename.format(row["canonical_dir_tif"], stem) + "\n")
+                rename_issue = RENAME.format(row["canonical_dir_tif"], stem) + "\n"
+                f_rename.write(rename_issue + "\n")
 
 
 def main():
-
-    # parse arguments
     args = parse_args()
 
-    extract_tif_via_bash(args.f_in)
+    create_bash_scripts(args.f_in, args.f_tet, args.f_rename)
 
 
 ################################################################################
